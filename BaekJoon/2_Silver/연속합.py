288394fcc14e08007be70c940ae3fac8bf740caf
wns312 <wns312@gmail.com>
2021-08-07 15:17:24 +0900

    백준 1912 : 연속합

---
+++ BaekJoon/2_Silver/연속합.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open('../input.txt', 'r')
+
+N = int(input())
+arr = list(map(int, input().split()))
+for i in range(1, N):
+    if arr[i - 1] > 0 and arr[i] + arr[i - 1] > arr[i]:
+        arr[i] = arr[i] + arr[i - 1]
+print(max(arr))
