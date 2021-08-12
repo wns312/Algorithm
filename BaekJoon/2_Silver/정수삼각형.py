389b11ea87f2aee389de52f7159bafa886218243
wns312 <wns312@gmail.com>
2021-08-12 19:52:47 +0900

    백준 1932: 정수삼각형

---
+++ BaekJoon/2_Silver/정수삼각형.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open('../input.txt', 'r')
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+for r in range(N-2, -1, -1): # 세로
+    for c in range(len(arr[r])): # 가로
+        arr[r][c] += max(arr[r+1][c],arr[r+1][c+1])
+
+print(arr[0][0])
